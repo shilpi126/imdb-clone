f5e5dd3d760655cded71e72d0c04aff95d06bbc7
shilpi <shilpi8624@gmail.com>
2023-04-18 15:17:35 +0530

    "about page completed"

---
+++ src/app/about/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-5'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic delectus earum ab quisquam a. Obcaecati ad fuga odio veniam atque quasi nesciunt in veritatis voluptatem consequuntur aut iusto laborum ipsa rem eveniet, minima temporibus, ipsum accusantium, quas aspernatur amet maiores asperiores. Maxime maiores sint dolore dolores autem non tenetur mollitia 
+        </p>
+
+        <p>
+        obcaecati earum. Fugiat odit ea recusandae ad exercitationem facere eveniet explicabo? Consectetur error praesentium consequatur laboriosam. Ducimus quibusdam totam incidunt saepe eaque nesciunt voluptatum alias nostrum? Sunt, porro vero laboriosam quos suscipit ea beatae eaque numquam voluptatum ducimus pariatur sequi doloremque aperiam enim qui sint quisquam culpa illum mollitia itaque expedita nesciunt cum dolores facilis? Veritatis dolores esse 
+        </p>
+
+        <p>eveniet doloremque expedita, distinctio eos in odio veniam! Aspernatur dolore rerum ipsa fugiat sapiente eos praesentium enim. Enim commodi quod, eius voluptatum sapiente deserunt natus! Explicabo, quaerat rerum aut enim aspernatur labore, dicta maxime minima perferendis fuga fugiat rem beatae assumenda facere nesciunt nobis deleniti ipsa officia obcaecati. Deleniti porro rerum ipsa voluptate eos repellendus mollitia non exercitationem, sed consequuntur officiis numquam odit incidunt molestias vero omnis adipisci qui! </p>
+    </div>
+  )
+}
+
+export default page
